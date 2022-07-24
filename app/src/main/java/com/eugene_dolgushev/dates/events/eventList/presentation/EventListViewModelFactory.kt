9dcb3b331486f6438e834e7f201993ed692a1abe
Dolgushev Eugene <baraxlush1995@gmail.com>
2022-07-24 22:35:10 +0300

    Рефакторинг

--- app/src/main/java/com/eugene_dolgushev/dates/events/eventList/presentation/EventListViewModelFactory.kt
+++ app/src/main/java/com/eugene_dolgushev/dates/events/eventList/presentation/EventListViewModelFactory.kt
@@ -1,6 +1,6 @@
 package com.eugene_dolgushev.dates.events.eventList.presentation
 
-import com.eugene_dolgushev.dates.getViewModel
+import com.eugene_dolgushev.dates.utils.extensions.getViewModel
 
 fun EventListFragment.viewModel(
 ): EventListViewModel {
--- app/src/main/java/com/eugene_dolgushev/dates/utils/extensions/ViewModelExt.kt
+++ app/src/main/java/com/eugene_dolgushev/dates/utils/extensions/ViewModelExt.kt
@@ -1,4 +1,4 @@
-package com.eugene_dolgushev.dates
+package com.eugene_dolgushev.dates.utils.extensions
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
